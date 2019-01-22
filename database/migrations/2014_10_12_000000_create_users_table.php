67561c80859a0dec5d2afc8ada2066a413827140
Mazharul Islam <mazharulslm9@gmail.com>
2019-01-22 12:16:10 +0600

    User migration modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',128);
+            $table->string('email',100)->unique();
+            $table->string('phone_number')->unique();
+            $table->string('email_verification_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('facebook_id',150)->nullable();
+            $table->string('google_id',150)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
